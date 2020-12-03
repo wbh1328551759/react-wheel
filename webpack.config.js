990cf6f26f05e0eea765437de6e6f6e72f0e021a
吴wbh1328551759 <1328551759@qq.com>
2020-12-04 00:19:52 +0800

    add sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,34 @@
-const path = require("path");
+const path = require('path')
 module.exports = {
   entry: {
-    index: "./lib/index.tsx", //入口是 index.tsx
+    index: './lib/index.tsx', //入口是 index.tsx
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
   },
   output: {
-    path: path.resolve(__dirname, "dist/lib"), //输出的目录
-    library: "wr-ui-wheel", //库的名字
-    libraryTarget: "umd", //库的格式
+    path: path.resolve(__dirname, 'dist/lib'), //输出的目录
+    library: 'wr-ui-wheel', //库的名字
+    libraryTarget: 'umd', //库的格式
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/, //用于匹配 tsx 格式文件
-        loader: "awesome-typescript-loader", //将 tsx 文件翻译为 js
+        loader: 'awesome-typescript-loader', //将 tsx 文件翻译为 js
       },
       {
         test: /\.svg$/,
-        loader: "svg-sprite-loader",
+        loader: 'svg-sprite-loader',
       },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
     ],
   },
-};
+}
