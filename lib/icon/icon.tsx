7f3d719d9b774c37352d2ff25c3b43fb3f6702f9
吴wbh1328551759 <1328551759@qq.com>
2020-12-06 19:46:14 +0800

    add icon.unit test

--- lib/icon/icon.tsx
+++ lib/icon/icon.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './importIcons';
-import classes from './helpers/classes';
+import classes from '../helpers/classes';
+
 interface IconProps extends React.SVGAttributes<SVGElement> {
   name: string;
 }
 
 const Icon: React.FC<IconProps> = ({className, name, ...restProps}: IconProps) => {
   return (
-    <svg className={classes('wr-icon', className)}
-         {...restProps}>
+    <svg className={classes('wr-icon', className)} {...restProps}>
       <use xlinkHref={`#${[name]}`}/>
     </svg>
   );
