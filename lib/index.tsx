1415ffe80b80d2c3305ab21f7a1c519d6e7fa664
吴wbh1328551759 <1328551759@qq.com>
2020-12-11 18:26:55 +0800

    auto publish

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,17 +1,1 @@
-import React from "react";
-import ReactDom from "react-dom";
-// import Button from "./button";
-import Icon from "./icon/icon";
-
-ReactDom.render(
-  <div>
-    <Icon name="wechat"
-          // className='ffffff'
-          onClick={(e) => console.log(e)}
-          onMouseDown={() => console.log('mouseDown')}
-          onMouseMove={() => console.log('move')}/>
-    <Icon name="alipay" />
-    <Icon name="QQ" />
-  </div>,
-  document.querySelector("#root")
-);
+export {default as Icon} from './icon/icon'
