0f36a1ee4fe56605794d50f80fd86b54e4077783
wbh1328551759 <1328551759@qq.com>
2020-12-28 23:48:04 +0800

    update dialog

---
+++ lib/dialog/Wrapper.tsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+export const Mask = styled.div`
+  &.wr-ui-dialog-mask {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    background: rgba(0, 0, 0, 0.5);
+  }
+`;
+
+export const Content = styled.div`
+  &.wr-ui-dialog {
+    position: fixed;
+    min-width: 20em;
+    border-radius: 4px;
+    background: white;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    .wr-ui-dialog-close {
+      position: absolute;
+      bottom: 100%;
+      left: 100%;
+      width: 1.5em;
+      height: 1.5em;
+      border-radius: 50%;
+      background: white;
+      transform: translate(-50%, 50%);
+
+      svg {
+        width: 1.5em;
+        height: 1.5em;
+      }
+    }
+
+    .wr-ui-dialog-header {
+      font-size: 22px;
+      padding: 8px 16px;
+      border-bottom: 1px solid grey;
+    }
+
+    .wr-ui-dialog-main {
+      min-height: 6em;
+      padding: 8px 16px;
+    }
+
+    .wr-ui-dialog-footer {
+      border-top: 1px solid grey;
+      padding: 8px 16px;
+      display: flex;
+      justify-content: flex-end;
+    }
+  }
+`;
--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,16 +1,70 @@
-import React, {useState} from 'react';
-import Dialog from './dialog'
+import React, { useState } from "react";
+import Dialog from "./dialog";
 
-function dialogExample(){
-  const [isVisible, setVisible] = useState<boolean>(false)
+function dialogExample() {
+  const [isVisible, setVisible] = useState<boolean>(false);
+  const [x, setX] = useState<boolean>(false);
   return (
     <div>
-      <button onClick={() => setVisible(!isVisible)}>click</button>
-      <Dialog visible={isVisible}>
-        <h1>1111</h1>
-      </Dialog>
+      <div>
+        <h1>example 1</h1>
+        <button onClick={() => setVisible(!isVisible)}>click</button>
+        <Dialog
+          visible={isVisible}
+          buttons={[
+            <button
+              onClick={() => {
+                setVisible(false);
+              }}
+            >
+              1
+            </button>,
+            <button
+              onClick={() => {
+                setVisible(false);
+              }}
+            >
+              2
+            </button>,
+          ]}
+          onClose={() => {
+            setVisible(false);
+          }}
+        >
+          <div>1111</div>
+        </Dialog>
+      </div>
+      <div>
+        <h1>example 2</h1>
+        <button onClick={() => setX(!x)}>click</button>
+        <Dialog
+          visible={x}
+          buttons={[
+            <button
+              onClick={() => {
+                setX(false);
+              }}
+            >
+              1
+            </button>,
+            <button
+              onClick={() => {
+                setX(false);
+              }}
+            >
+              2
+            </button>,
+          ]}
+          onClose={() => {
+            setX(false);
+          }}
+          closeOnClickMask={false}
+        >
+          <div>1111</div>
+        </Dialog>
+      </div>
     </div>
-  )
+  );
 }
 
-export default dialogExample
+export default dialogExample;
