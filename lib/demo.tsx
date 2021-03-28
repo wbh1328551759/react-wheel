fc581a3884f51d0211924a11946d061668005508
吴wbh1328551759 <1328551759@qq.com>
2021-03-28 23:10:13 +0800

    add doc script and build example.html

--- lib/demo.tsx
+++ lib/demo.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Highlight, { defaultProps } from 'prism-react-renderer';
 
-interface Props{
+interface Props {
   code: string;
   children: React.ReactElement;
 }
 
 const Demo: React.FC<Props> = ({code, children}: Props) => {
+  const [codeVisible, setCodeVisible] = useState<boolean>(false);
+  const codeContent = (
+    <Highlight {...defaultProps} code={code} language='jsx'>
+      {({className, style, tokens, getLineProps, getTokenProps}) => (
+        <pre className={className} style={style}>
+          {tokens.map((line, i) => (
+            <div {...getLineProps({line, key: i})}>
+              {line.map((token, key) => {
+                return <span {...getTokenProps({token, key})}/>;
+              })}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  );
   return (
     <div>
-      {children}
-      <pre>{code}</pre>
+      <div className="example">
+        {children}
+      </div>
+      <div>
+        <button onClick={() => setCodeVisible(!codeVisible)}>查看代码</button>
+      </div>
+      {codeVisible && codeContent}
     </div>
-  )
-}
+  );
+};
 
-export default Demo
+export default Demo;
---
+++ webpack.config.doc.js
@@ -0,0 +1,21 @@
+const base = require("./webpack.config");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+
+module.exports = Object.assign({}, base, {
+  mode: "production", //设置为生产模式，development是开发模式
+  entry: {
+    ...base.entry,
+    example: './example.tsx', //入口是 index.tsx
+  },
+  output: {
+    path: path.resolve(__dirname, 'doc'), //输出的目录
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "WR-UI",
+      template: "example.html",
+      filename: "example.html"
+    }),
+  ],
+});
